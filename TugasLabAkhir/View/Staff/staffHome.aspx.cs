d3f9609a53dd421a6cb54761e8df5ff096af7549
LuckyBastard <rafi.khairunizham@gmail.com>
2023-05-16 01:56:45 +0700

    Add Customer And Staff Navbar

---
+++ TugasLabAkhir/View/Staff/staffHome.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace TugasLabAkhir.View.Staff
+{
+    public partial class staffHome : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
