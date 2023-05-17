25d80353b58f4425eb2e7df619bd68c7e3a39a31
LuckyBastard <rafi.khairunizham@gmail.com>
2023-05-17 21:05:38 +0700

    Adding userFactory

---
+++ TugasLabAkhir/Factory/userFactory.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using TugasLabAkhir.Model;
+
+namespace TugasLabAkhir.Factory
+{
+    public class userFactory
+    {
+        public static User registUser(string name, string email, string gender, string password, string role)
+        {
+            User u = new User();
+
+            u.userName = name;
+            u.userEmail = email;
+            u.userGender = gender;
+            u.userPassword = password;
+            u.roleID = int.Parse(role);
+
+            return u;
+        }
+
+    }
+}
+}
